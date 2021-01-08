2c5ebb9e9dec9eef4201bfce85c2fa200b36683d
PastVision <anuragtrivedi96@live.com>
2021-01-08 15:10:33 +0530

    ReadFile

--- practice/main.py
+++ practice/main.py
@@ -4,5 +4,8 @@ if __name__ == '__main__':
     if len(sys.argv)<2:
         print('Usage: main.py filename')
     else:
-        with open(sys.argv[1],'rt') as f:
-            pass
+        pizzas = list()
+        with open(sys.argv[1],'rt') as f:           #Set to Open a_example in launch.json
+            m, t2, t3, t4 = map(int, f.readline().split())
+            pizzas = [x.rstrip('\n') for x in f.readlines()]            #Will be optimized later
+            print(pizzas)
